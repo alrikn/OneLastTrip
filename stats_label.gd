7d29cc8906cc05be99c63402f44b2635b62028ac
Alrik Neihouser <alrik.neihouser@epitech.eu>
2025-11-14 18:53:35 +0100

    feat: stats panels

---
+++ stats_label.gd
@@ -0,0 +1,14 @@
+extends Label
+
+@onready var player_state = $"../../../Player/PlayerState"
+
+func _physics_process(_delta):
+    if player_state == null:
+        text = "PlayerState not found"
+        return
+
+    text = "Velocity: %s\nRotation: %.2f\nHeight: %.1f" % [
+        player_state.linear_vel,
+        player_state.angular_vel,
+        player_state.height
+    ]
--- value_calculator.gd
+++ value_calculator.gd
@@ -2,7 +2,7 @@ extends Node
 
 @export var player: RigidBody2D
 
-var linear_vel: Vector2
+var linear_vel: float
 var angular_vel: float
 var height: float
 
@@ -10,7 +10,7 @@ func _physics_process(_delta):
     if not is_instance_valid(player):
         return
 
-    linear_vel = player.linear_velocity
+    linear_vel = (abs(player.linear_velocity[0]) + abs(player.linear_velocity[1]))
     angular_vel = player.angular_velocity
     height = -player.position.y
     print("linear_vel = %s, angular_vel = %s, height = %s" % [linear_vel, angular_vel, height])
