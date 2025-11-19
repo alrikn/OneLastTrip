d48db249f2bf293db7b64c80213097594943892d
Alrik Neihouser <alrik.neihouser@epitech.eu>
2025-11-19 11:15:19 +0100

    fix: nicer displays

--- stats_label.gd
+++ stats_label.gd
@@ -7,7 +7,7 @@ func _physics_process(_delta):
         text = "PlayerState not found"
         return
 
-    text = "Velocity: %s\nRotation: %.2f\nHeight: %.1f" % [
+    text = "Velocity: %.2f\nRotation: %.2f\nHeight: %.1f" % [
         player_state.linear_vel,
         player_state.angular_vel,
         player_state.height
--- value_calculator.gd
+++ value_calculator.gd
@@ -5,12 +5,13 @@ extends Node
 var linear_vel: float
 var angular_vel: float
 var height: float
+const PIXELS_PER_METER = 100.0
 
 func _physics_process(_delta):
     if not is_instance_valid(player):
         return
-
-    linear_vel = (abs(player.linear_velocity[0]) + abs(player.linear_velocity[1]))
+    #lets call 100 pixels 1 meter (or smth)
+    linear_vel = player.linear_velocity.length() / PIXELS_PER_METER
     angular_vel = player.angular_velocity
-    height = -player.position.y
-    print("linear_vel = %s, angular_vel = %s, height = %s" % [linear_vel, angular_vel, height])
+    height = -player.position.y  / PIXELS_PER_METER
+    #print("linear_vel = %s, angular_vel = %s, height = %s" % [linear_vel, angular_vel, height])
