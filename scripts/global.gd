af7bdf5ece7a3dc101257c4a0b40e62bb96bb47f
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-06-05 20:44:06 +0200

    feat: reset_game function

---
+++ scripts/global.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var time_left: float = 300.0
+var score: int = 0
+var is_game_active: bool = false
+# There's a ton of other variables we could add here: max_people, max_platforms, etc.
+# But for now, we'll just keep it simple.
+
+func reset_game():
+    time_left = 300.0
+    score = 0
+    is_game_active = true
