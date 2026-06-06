391ef3771712315033a077fdf9e927319f47723a
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-06-06 14:12:16 +0200

    feat: working menu with start button

--- scripts/stats_label.gd
+++ scripts/stats_label.gd
@@ -3,13 +3,12 @@ extends Label
 @onready var player_state = $"../../../Player/PlayerState"
 
 func _physics_process(_delta):
-       if player_state == null:
-              text = "PlayerState not found"
-              return
-
-       text = "Velocity: %.2f\nRotation: %.2f\nDistance: %.1f\nHeight: %.1f" % [
-              player_state.linear_vel,
-              player_state.angular_vel,
-              player_state.distance,
-              player_state.height,
-       ]
+	if player_state == null:
+		text = "PlayerState not found"
+		return
+	text = "Velocity: %.2f\nRotation: %.2f\nDistance: %.1f\nHeight: %.1f" % [
+		player_state.linear_vel,
+		player_state.angular_vel,
+		player_state.distance,
+		player_state.height,
+	]
