7b4746823597b5853d05f32099daad22566e30ae
Alrik Neihouser <alrik.neihouser@epitech.eu>
2026-06-06 12:50:40 +0200

    fix: spaces to tabs

--- scripts/stats_calculator.gd
+++ scripts/stats_calculator.gd
@@ -11,19 +11,19 @@ var g_force := 0.0
 const PIXELS_PER_METER = 100.0
 
 func _physics_process(_delta):
-	if not is_instance_valid(player):
-		return
-	var current_velocity = player.linear_velocity / PIXELS_PER_METER
-	linear_vel = player.linear_velocity.length() / PIXELS_PER_METER
-	angular_vel = player.angular_velocity
-	height = -player.position.y  / PIXELS_PER_METER
-	distance = player.position.x / PIXELS_PER_METER
-	var acceleration = (current_velocity - previous_velocity)# / delta #dlta is time change. here it is 1 tick so i am not putting it
-	previous_velocity = current_velocity
-	var accel_magnitude = acceleration.length()
+       if not is_instance_valid(player):
+              return
+       var current_velocity = player.linear_velocity / PIXELS_PER_METER
+       linear_vel = player.linear_velocity.length() / PIXELS_PER_METER
+       angular_vel = player.angular_velocity
+       height = -player.position.y  / PIXELS_PER_METER
+       distance = player.position.x / PIXELS_PER_METER
+       var acceleration = (current_velocity - previous_velocity)# / delta #dlta is time change. here it is 1 tick so i am not putting it
+       previous_velocity = current_velocity
+       var accel_magnitude = acceleration.length()
 
-	if (accel_magnitude > 6):
-		player.explode()
-		print("accel_magnitude = %.2f" % [accel_magnitude])
+       if (accel_magnitude > 6):
+              player.explode()
+              print("accel_magnitude = %.2f" % [accel_magnitude])
 
-	#print("linear_vel = %s, angular_vel = %s, height = %s" % [linear_vel, angular_vel, height])
+       #print("linear_vel = %s, angular_vel = %s, height = %s" % [linear_vel, angular_vel, height])
