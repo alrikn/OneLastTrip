014c783a519e3331811386fe6c427b7d8eab020f
Nicolas <183367651+Nvcoo@users.noreply.github.com>
2026-06-06 14:01:51 +0200

    fix: accepted incoming changes

--- scripts/stats_label.gd
+++ scripts/stats_label.gd
@@ -3,18 +3,6 @@ extends Label
 @onready var player_state = $"../../../Player/PlayerState"
 
 func _physics_process(_delta):
-<<<<<<< HEAD
-	if player_state == null:
-		text = "PlayerState not found"
-		return
-
-	text = "Velocity: %.2f\nRotation: %.2f\nDistance: %.1f\nHeight: %.1f" % [
-		player_state.linear_vel,
-		player_state.angular_vel,
-		player_state.distance,
-		player_state.height,
-	]
-=======
        if player_state == null:
               text = "PlayerState not found"
               return
@@ -25,4 +13,3 @@ func _physics_process(_delta):
               player_state.distance,
               player_state.height,
        ]
->>>>>>> origin/main
