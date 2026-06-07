d22ccf27d5cf094e944a2f0d8854f44c3a3b2bee
Alrik Neihouser <alrik.neihouser@epitech.eu>
2026-06-07 15:01:30 +0200

    fix: win is now set to false

--- scripts/global.gd
+++ scripts/global.gd
@@ -14,6 +14,8 @@ func reset_game():
 	time_left = 300.0
 	score = 0
 	is_game_active = true
+    win = false
+    #we also need to respawn all the little poeple somehow.
 
 func _process(delta: float):
 	if is_game_active:
